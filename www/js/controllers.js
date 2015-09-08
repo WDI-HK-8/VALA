7c708c15983fbd3d410111818d75e663f691c141
jonycfu <jon.yc.fu@gmail.com>
2015-09-09 01:51:05 +0800

    feat(users-signin): Add basic user signup and signin feature

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,21 +1,29 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, $auth) {
 })
 
-.controller('landingCtrl', function($scope) {
+.controller('landingCtrl', function($scope, $auth) {
   $scope.signinForm = {};
 
   $scope.signin = function(){
-
+    $auth.submitLogin($scope.signinForm).then(function(response){
+      console.log(response);
+    }).catch(function(response){
+      console.log(response);
+    })
   }
 })
 
-.controller('signupCtrl', function($scope) {
+.controller('signupCtrl', function($scope, $auth) {
   $scope.signupForm = {};
 
   $scope.signup = function(){
-
+    $auth.submitRegistration($scope.signupForm).then(function(response){
+      console.log(response);
+    }).catch(function(response){
+      console.log(response);
+    })
   }
 });
 
